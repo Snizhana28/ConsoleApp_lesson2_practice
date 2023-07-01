09917a172a066898d3be56a917214574211aacaa
snizhana28 <snizhana2812kostyuk@gmail.com>
2023-07-01 15:31:20 +0300

    Task2

--- Program.cs
+++ Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(" ");
+            /*Створіть додаток, який відображає кількість значень у 
+            масиві менше заданого параметра користувачем. Наприклад, 
+            кількість значень менших, ніж 7 (7 введено користувачем 
+            з клавіатури).*/
+            int[] array = new int[10];
+            int count = 0;
+            int number = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = i;
+            }
+            Console.WriteLine("Enter number");
+            number = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] < number)
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine($"Count of numbers less than {number} is {count}");
         }
     }
 }
