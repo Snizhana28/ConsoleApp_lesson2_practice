c347f1d5c36b54a525e758a1a82eb8f44a5dd65f
snizhana28 <snizhana2812kostyuk@gmail.com>
2023-07-01 14:42:15 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleApp_lesson2_practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
