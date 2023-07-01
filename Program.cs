c7519f430a4ddb548b8c157e73ddbe5316f39e33
snizhana28 <snizhana2812kostyuk@gmail.com>
2023-07-01 15:39:34 +0300

    Done task3

--- Program.cs
+++ Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(" ");
+            int[] array = { 7, 6, 5, 3, 4, 7, 6, 5, 8, 7, 6, 5 };
+            int[] input = new int[3];
+            int count = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                Console.Write($"Enter {i + 1} number: ");
+                input[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == input[0] && array[i + 1] == input[1] && array[i + 2] == input[2])
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine($"Count of sequence: {count}");
         }
     }
 }
