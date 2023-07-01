e499f64ab3ca76f13089984ff1a842ced5449e93
snizhana28 <snizhana2812kostyuk@gmail.com>
2023-07-01 17:07:29 +0300

    Done task7

--- Program.cs
+++ Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(" ");
+            //sun cat dogs cup tea;
+            //nus tac sgod puc aet
+            Console.Write("Enter a sentence : ");
+            string sentence = Console.ReadLine();
+            string[] words = sentence.Split(new char[] { ' ', '\t', '\n', '\r', '.', ',', ';', '!', '?' }, StringSplitOptions.RemoveEmptyEntries);
+            for (int i = 0; i < words.Length; i++)
+            {
+                char[] word = words[i].ToCharArray();
+                Array.Reverse(word);
+                words[i] = new string(word);
+            }
+            sentence = string.Join(" ", words);
+            Console.WriteLine("Reversed sentence : " + sentence);
+            Console.ReadLine();
         }
     }
 }
