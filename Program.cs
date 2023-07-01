547bc114cceb5d6182aec2bf3bd08790929e6a5f
snizhana28 <snizhana2812kostyuk@gmail.com>
2023-07-01 16:45:25 +0300

    Done Task5

--- Program.cs
+++ Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(" ");
+            int[,] array = new int[5, 5];
+            Random random = new Random();
+            int min = 0;
+            int max = 0;
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLongLength(1); j++)
+                {
+                    array[i, j] = random.Next(0, 100);
+                    Console.Write(array[i, j] + " ");
+                    if (array[i, j] < min)
+                    {
+                        min = array[i, j];
+                    }
+                    if (array[i, j] > max)
+                    {
+                        max = array[i, j];
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine($"Min value : {min}");
+            Console.WriteLine($"max value : {max}");
         }
     }
 }
