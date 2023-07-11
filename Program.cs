e8943ed8b43e37880197ee3cdaed47fb05d2dbc9
snizhana28 <snizhana2812kostyuk@gmail.com>
2023-07-11 14:16:25 +0300

    Done task 9

--- Program.cs
+++ Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(" ");
+            Console.WriteLine("Enter a sentence : ");
+            string sentence = Console.ReadLine();
+            Console.WriteLine("Enter a word to search : ");
+            string search = Console.ReadLine();            
+            int count = 0;
+            int startIndex = 0;
+
+            while ((startIndex = sentence.IndexOf(search, startIndex)) != -1)
+            {
+                count++;
+                startIndex += search.Length;
+            }
+            Console.WriteLine("Result : " + count);
         }
     }
 }
